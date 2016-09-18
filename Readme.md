cecc3b78ec68bc3824ecba8e78a2a23368563284
Ashish Negi <ashish@quintype.com>
2016-09-18 23:55:14 +0530

    Initial commit with some assignments after 1st class.

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# HSClass : Haskell Class with friends..
+
+## Setup :
+1. Install Stack : `brew install haskell-stack` . For brew see: `http://brew.sh/`
+2. `stack new your-project-name`
+4. `stack build --fast --file-watch` # compiles automatically the code changes.
+3. start hacking in app/Main.hs
---
+++ app/Main.hs
@@ -0,0 +1,32 @@
+module Main where
+
+toDigits :: Integer -> [Integer]
+toDigits n = undefined
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = undefined
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther list = undefined
+
+sumDigits :: [Integer] -> Integer
+sumDigits list = undefined
+
+validate :: Integer -> Bool
+validate n = undefined
+
+main :: IO ()
+main = do
+  putStrLn "Hello world"
+  -- show takes a type and convert to string
+  putStrLn $ show 1
+  putStrLn $ show [1 , 2 , 3]
+  putStrLn $ show True
+
+
+
+-- syntax :
+ifSyntax num = if num > 0
+               then num - 1
+               else num + 1
+
