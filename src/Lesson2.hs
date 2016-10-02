33e8219c983fb9a374a1c03ef2fd2d210f88dd79
Ashish Negi <ashish@quintype.com>
2016-10-03 00:59:50 +0530

    Adds more comment while in class

--- src/Lesson2.hs
+++ src/Lesson2.hs
@@ -8,20 +8,46 @@ module Lesson2 where
 -- types can be written in 3 ways
 -- 1. type : gives another name to a type
 --    makes it readable.
-type NumThreads = Int
+-- type alias
+type NumThreads1 = Int
+type MaxDepthToCrawl1 = Int
+
+foo :: NumThreads1 -> MaxDepthToCrawl1 -> Int
+foo numThreads depth = numThreads
+
+v = foo 0 1
+-- compiler will not give any error :
+-- CrawlConfig 1 0 ;; CrawlConfig 0 1
+-- foo :: NumThreads -> ...
+-- compiler will give error :
 
 -- 2. data : to make a type
+-- LHS = RHS
+-- LHS User => Type COnstructor
+-- RHS User => Data Constructor
+-- Values of User => Values (name) * Values (age)
+-- Product Types / Sum types
 data User = User
-            { name :: String
+            { name :: String -- name :: User -> String
             , age  :: Int
             }
+
 -- note: this `{ .. }` is also called Record Syntax.
 
 -- another example of `data`
+-- Enumeration only.. // Union Type // Sum types
+-- Value is Run time // Type is compile time
+-- Day is Monday OR Tuesday OR ....
 data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 -- 3. since "data" makes a wrapper over basic data type,
 --    we can use "newtype" for getting benefit of types but no performance penalty
 newtype Msg = Msg String
+newtype NumThreads = NumThreads Int
+-- CrawlConfig (NumThreads 1) (MaxDepthToCrawl 1) ;;
+-- CrawlConfig 1 1 ;; Type error while creating values..
+-- age :: [ Int ] -- [ Age ] -- [ Int ]
+-- Boxing / UnBoxing
+-- CrawlConfig (MaxDepthToCrawl 1) (NumThreads 1)
 
 -- If i give one `|` between messages i get the error
 -- newtype Msg2 = Msg2 String | Msg3 String String
@@ -35,9 +61,26 @@ newtype Msg = Msg String
 -- data gives us type helps and multiple data constructors.
 
 -- Just like template classes in haskell, we can write
-data List a = List a (List a) | Empty
+data List a = List    a      (List a) | Empty
+              -- List 1  Empty
+              -- List 1  (List 2 Empty)
+              -- List 1  (List 2 (List 3 (List a)))
+
+-- List needs a Type (Int/String) => new Type (List Int / List String)
+-- List a is constructed by (List a ) and has More List type
+-- List Int = List Int (List Int) | Empty
+-- List String = List String (List String) | Empty
+
 -- this can be written as
+-- LHS                     =  RHS
 data ListTypeConstructor a = ListDataConstructor a (ListTypeConstructor a) | EmptyC
+data ListTC a = ListDC a (ListTC a) | EmptyCs
+                -- EmptyCs
+                -- ListDC 1  EmptyCs
+                -- -- ListDC 1  (ListDC 2 (ListTC a)) -- REMOVE all TC / TypeConstructors
+                -- ListDC 1  (ListDC 2 (ListDC 3 EmptyCs))
+-- foo :: ListTypeConstructor Int -> Int
+-- foo list = let listOfInts = ListDataConstructor 1 EmptyC
 
 -- so what is type constructor
 -- data constructor is used for creating actual data (run time)
@@ -63,10 +106,18 @@ fooThreads a = [1,2,3]
 -- fooThreds2 :: Int -> [Int] ???
 -- types are cheap, always give a name
 
+-- data User = User
+--             { name :: String -- name :: User -> String
+--             , age  :: Int    -- age  :: User -> Int
+--             }
+
 -- see the pattern matching / destructuring
 fooUserName :: User -> String
 fooUserName (User name age) = name
+-- name is a function actually
+-- fooUserName user = (name user)
 
+-- data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 -- what when you have `|` i.e. multiple DataConstructors,
 -- you can not pattern match on the argument. You need to use `case <argument> of`
 fooDayToInt :: Day -> Int
@@ -86,6 +137,30 @@ fooDayToInt day =
 fooMsgToString :: Msg -> String
 fooMsgToString (Msg s) = s
 
+data Book = Book Int
+data Author = Author Book
+-- Partial Function is NOT GOOD / BAD
+-- increment :: Int -> Int
+-- increment a = a + 1
+
+-- Partial Function is NOT GOOD / BAD
+-- authorFactory :: Book -> Author
+-- authorFactory b@(Book bId) = if bId < 0
+--                              then NoAuthor
+--                              else (Author b)
+-- bookFactory :: Int -> Book
+-- bookFactory bId = if bId < 0
+--                   then NoBook
+--                   else (Book bId)
+
+-- Author AuthorFactory(Book book) {
+--    if (book.id < 0) {
+--      throw IllegalAEce... ();
+--      return null;
+--      }
+--    return new Author(book);;
+-- }
+
 
 -- So this is syntax..
 -- lets start the philosophy :)
@@ -95,7 +170,27 @@ fooMsgToString (Msg s) = s
 -- in java, if our arguments are wrong, we can return null and that caller-of-function can take as
 -- failure of function.. but since we have only Values in haskell, this is not possible..
 -- so ??? create a type :)
+
 data Result value = Ok value | Failed
+     -- Result Author
+     -- Result Book
+authorFactory2 :: Book -> Result Author
+authorFactory2 b@(Book bId) = if bId < 0
+                             then Failed
+                             else Ok (Author b)
+
+bookFactory2 :: Int -> Result Book
+bookFactory2 bId = if bId < 0
+                  then Failed
+                  else Ok (Book bId)
+-- api route --- db call --
+-- foo2 = let resultOfAuthor = authorFactory2 (Book 1) -- Result Author
+--       in case resultOfAuthor of
+--           Failed ->  Failed
+--           Ok author -> fooAuthor author
+
+-- fooAuthor :: Author -> Book
+-- fooAuthor (Author book) = book
 
 -- This is thinking in terms of Types.. :)
 
